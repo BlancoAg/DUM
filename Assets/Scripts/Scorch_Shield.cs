76cf07cbd6db7b676417bdd3adcac7c706105ca1
BlancoAg <agustinbla12@gmail.com>
2023-01-03 00:21:57 -0300

    scorch shield works like a switch

--- Assets/Scripts/Scorch_Shield.cs
+++ Assets/Scripts/Scorch_Shield.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Scorch_Shield : MonoBehaviour
 {
-    bool shielded = false;
+    public bool shielded = false;
     
     // Start is called before the first frame update
     void Start()
@@ -15,18 +15,10 @@ public class Scorch_Shield : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-    if(Input.GetKey("t")){
-        Debug.Log("t");
-        shielded = true;
-    }else{
-       shielded = false;
-    }
-    
-    if(shielded){
-        Debug.Log("shielded");
-    }else{
-       Debug.Log("Normal");
-    }
+        if (Input.GetKeyDown("t"))
+        {
+            shielded = !shielded;
+        }
     }
 }
+

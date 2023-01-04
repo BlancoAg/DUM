b733a540cff10288a96a0bcae4a47dfcd0278368
BlancoAg <agustinbla12@gmail.com>
2023-01-03 22:09:54 -0300

    pushes you down if used in the air

--- Assets/Scripts/StoneStance.cs
+++ Assets/Scripts/StoneStance.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class StoneStance : MonoBehaviour
 {
     public bool stoned = false;
+    public float downForce = 5.0f;
     private FirstPersonController firstPersonController;
+    private CharacterController characterController;
+    
     
     void Start()
     {
         firstPersonController = GetComponent<FirstPersonController>();
+        characterController = GetComponent<CharacterController>();
+
     }
 
     void Update()
@@ -17,6 +22,11 @@ public class StoneStance : MonoBehaviour
         if (Input.GetKeyDown("r"))
         {
             stoned = !stoned;
+        }
+
+        if (stoned && !characterController.isGrounded)
+        {
+            characterController.Move(Vector3.down * downForce);
         }
     }
 }

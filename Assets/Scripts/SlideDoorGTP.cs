55521534820802dae0226bb6ac801ec617832bda
kachan <56056839+kachan3@users.noreply.github.com>
2023-02-14 01:26:32 -0300

    DoorSlide created

--- Assets/Scripts/SlideDoorGTP.cs
+++ Assets/Scripts/SlideDoorGTP.cs
@@ -4,14 +4,15 @@ public class SlideDoorGTP : MonoBehaviour, IActivate
 {
     public Transform door;
     public float speed = 5.0f;
-    public Vector3 openPosition = new Vector3(0, -5, 0);
     public Vector3 closedPosition;
+    public Vector3 openPosition;
 
     private bool doorIsOpen = false;
 
     private void Start()
     {
         closedPosition = door.position;
+        openPosition = closedPosition + new Vector3(0,-6,0);
     }
 
     private void Update()

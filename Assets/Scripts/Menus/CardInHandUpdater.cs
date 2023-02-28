caa6ca9ec6ea3e8a3f5166a9a809fa3fac4b909d
BlancoAg <agustinbla12@gmail.com>
2023-02-28 12:07:13 -0300

    added card descriptions to the cards

--- Assets/Scripts/Menus/CardInHandUpdater.cs
+++ Assets/Scripts/Menus/CardInHandUpdater.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CardInHandUpdater : MonoBehaviour
 {
     public Hand handScript;
     public Image cardImage;
     public GameObject cardToCast;
+    public TMP_Text cardText;
 
     void Update()
     {
@@ -16,6 +18,11 @@ public class CardInHandUpdater : MonoBehaviour
             Transform quad = currentCard.transform.GetChild(3);
             Material material = quad.GetComponent<Renderer>().material;
             cardImage.material = material;
+
+            // Retrieve the TextMeshPro component from the 5th child of the current card
+            string cardTextMeshPro = currentCard.transform.GetChild(4).GetComponent<TMP_Text>().text;
+            // Update the text of the TextMeshPro object
+            cardText.text = cardTextMeshPro;
         }
     }
 }

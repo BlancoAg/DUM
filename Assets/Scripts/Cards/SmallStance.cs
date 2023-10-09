c7397f08cec0f10aa8cfa872377c1609622a6172
BlancoAg <agustinbla12@gmail.com>
2023-10-09 00:38:20 -0300

    fixed puppet test room

--- Assets/Scripts/Cards/SmallStance.cs
+++ Assets/Scripts/Cards/SmallStance.cs
@@ -18,7 +18,6 @@ public class SmallStance : MonoBehaviour, ICard
 
     public void cast_card(GameObject handGameObject)
     {
-<<<<<<< Updated upstream
         // Find the PlayerMainScript component on the GameObject from the Hand script
         var player = handGameObject.GetComponent<PlayerMainScript>();
 
@@ -26,13 +25,6 @@ public class SmallStance : MonoBehaviour, ICard
         {
             player.change_size();
             ready = false;
-=======
-        
-        var player = GameObject.Find("Player").GetComponent<PlayerMainScript>();
-        if(ready){
-        player.change_size();
-        ready = false;
->>>>>>> Stashed changes
         }
     }
 }

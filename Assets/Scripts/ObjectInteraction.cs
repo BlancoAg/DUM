1e2d4097eb3af2a06ead4b99157ef641c477f00a
BlancoAg <agustinbla12@gmail.com>
2023-10-18 00:51:02 -0300

    fixed puppet interaction
    puppets are accessed by ObjectInteraction instead of PuppetController

--- Assets/Scripts/ObjectInteraction.cs
+++ Assets/Scripts/ObjectInteraction.cs
@@ -6,16 +6,20 @@ public class ObjectInteraction : MonoBehaviour
     DialogueSystem dialoguesystem;
     QuestTrigger questTrigger;
     private List<string> dialogues;
+    private PuppetController puppetCont;
+    public PuppetController targetPuppet;
 
     private void Start()
     {
         dialoguesystem = GetComponent<DialogueSystem>(); // Remove "gameObject."
+        puppetCont = GetComponent<PuppetController>();
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            Camera cameraComponent = GetComponentInChildren<Camera>();
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             
@@ -33,6 +37,16 @@ public class ObjectInteraction : MonoBehaviour
                 {
                     // Call the "Trigger" method if it exists
                     questTrigger.Trigger();
+                }
+            }
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.CompareTag("Puppet") || hit.collider.CompareTag("Player"))
+                {
+                    // Store the target puppet for control transfer
+                    PuppetController targetPuppet = hit.collider.gameObject.GetComponent<PuppetController>();
+                    puppetCont.ToggleControl(false);
+                    targetPuppet.ToggleControl(true);
                 }
             }
         }
--- Assets/Scripts/PuppetController.cs
+++ Assets/Scripts/PuppetController.cs
@@ -4,83 +4,35 @@ using UnityEngine;
 
 public class PuppetController : MonoBehaviour
 {
-    public bool inControl = false;
+    public bool inControl;
     private Camera puppetCamera;
     private PlayerMovementTutorial playerMovement;
+    private ObjectInteraction objectInt;
     private Renderer meshRenderer;
-    private GameObject interfaz; // Reference to the Interfaz GameObject.
+    private GameObject interfaz; 
     private Hand handScript;
     private GameObject gameObject;
     private GameObject cardUpdater;
 
-    // Variables to track control transfer
-    private bool isTransferringControl = false;
-    private PuppetController targetPuppet;
 
     void Start()
     {
         puppetCamera = GetComponentInChildren<Camera>();
         playerMovement = GetComponent<PlayerMovementTutorial>();
+        objectInt = GetComponent<ObjectInteraction>();
         meshRenderer = GetComponent<Renderer>();
-        interfaz = transform.Find("Interfaz").gameObject; // Find and store the Interfaz GameObject.
+        interfaz = transform.Find("Interfaz").gameObject; 
         handScript = GetComponent<Hand>();
         gameObject = transform.Find("GameObject").gameObject;
         cardUpdater = transform.Find("CardInHandUpdater").gameObject;
 
-        if (puppetCamera != null)
-        {
-            puppetCamera.enabled = true;
-        }
-        else
-        {
-            Debug.LogWarning("No Camera component found on this GameObject or its children.");
-        }
-
         ToggleControl(inControl);
     }
 
-    void Update()
-    {
-        if (!isTransferringControl && Input.GetMouseButtonDown(0))
-        {
-            Camera cameraComponent = GetComponentInChildren<Camera>();
-            Ray ray = cameraComponent.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (hit.collider.CompareTag("Puppet") || hit.collider.CompareTag("Player"))
-                {
-                    // Store the target puppet for control transfer
-                    targetPuppet = hit.collider.gameObject.GetComponent<PuppetController>();
-                    ToggleControl(false);
-                    targetPuppet.ToggleControl(true);
-                    // Start the control transfer coroutine
-                    //StartCoroutine(TransferControlAfterDelay(1.0f));
-                }
-            }
-        }
-    }
-
-    // IEnumerator TransferControlAfterDelay(float delay)
-    // {
-    //     isTransferringControl = true;
-
-    //     // Wait for the specified delay
-    //     yield return new WaitForSeconds(delay);
-
-    //     // Transfer control to the target puppet
-    //     ToggleControl(false);
-    //     targetPuppet.ToggleControl(true);
-
-    //     // Reset variables
-    //     targetPuppet = null;
-    //     isTransferringControl = false;
-    // }
-
-    void ToggleControl(bool control)
+    public void ToggleControl(bool control)
     {
         inControl = control;
+        objectInt.enabled = control;
 
         // Enable/disable the camera
         if (puppetCamera != null)
@@ -106,7 +58,7 @@ public class PuppetController : MonoBehaviour
             interfaz.SetActive(control);
         }
 
-         // Toggle the Hand script
+        // Toggle the Hand script
         if (handScript != null)
         {
             handScript.enabled = control;
